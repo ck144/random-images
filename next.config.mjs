837ea9942ec467f674adddeade3f5bd6c8d12c35
SUSHIL CK <09psushil@gmail.com>
2025-03-31 11:48:46 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // For static exports
+  distDir: 'build', // This matches Render's expectations
+  images: {
+    unoptimized: true, // Required for static exports
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
